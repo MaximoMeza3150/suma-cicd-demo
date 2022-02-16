e21cfe30d2ef7c4b3f6aba4059540814cb0db76b
MaximoMeza3150 <75499931+MaximoMeza3150@users.noreply.github.com>
2022-02-16 07:04:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,10 @@ const app = express()
 app.get('/', (req, res) =>{
     res.send("Hola mundo")
 })
+app.get('/suma/:a/:b',(req, res)=>{
+    let a = parseInt(req.param('a'))
+    let b = parseInt(req.param('b'))
+    res.send("resultado " + (a + b))
+})
 
 app.listen(process.env.PORT || 3000) //Puerto de heroku
